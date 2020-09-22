c3f824a58b4e8089c772c2a710e2fead6b43e326
Shivansh <gupta.shivansh98@gmail.com>
2020-09-22 10:02:57 +0530

    Added Django CRON and created a new app

--- youtube_fetch/settings.py
+++ youtube_fetch/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #External Package
+    'django_cron'
 ]
 
 MIDDLEWARE = [
