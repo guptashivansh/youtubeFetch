942d8ef146f9d8910fb3410b606a0a173c01309d
Shivansh <gupta.shivansh98@gmail.com>
2020-09-24 10:24:41 +0530

    Added ENV

--- youtube_fetch/settings.py
+++ youtube_fetch/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'xe(8tvsfcar)an#io@$i!_n%0t16dkc%rsgr2s28!)^un2epz9'
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
