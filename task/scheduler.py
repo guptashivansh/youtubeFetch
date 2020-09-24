b838aae87e048b33b07acbf1e5316002bfbb2c64
Shivansh <gupta.shivansh98@gmail.com>
2020-09-24 09:08:18 +0530

    Added support for multiple keys

--- task/scheduler.py
+++ task/scheduler.py
@@ -14,30 +14,36 @@ class fetchYoutubeVideoData(CronJobBase):
 
     def do(self):
         print('Hello World')
-        # AIzaSyBpHQLIyDjThRPlbfWvW-vV3hmn2locRjM
-        service = build('youtube', 'v3', developerKey='AIzaSyAPwUBHA2EYoFusv8KcXFUI7VFFZ2FXXSY')
-        result = service.search().list(part="snippet",q="how to make tea",type="video",order="date",publishedAfter="2010-01-01T00:00:00Z").execute()
-        # result = service.search().list(part="snippet",q="tea how",type="video",order="date").execute()
-        # result = service.search().list(part="snippet",q="tea how",type="video",order="date", publishedAfter="2019").execute()
-        # response = request.execute()
-        # print(result)
-        for item in result['items']:
-            video_id = item['id']['videoId']
-            publishedDateTime = item['snippet']['publishedAt']
-            title = item['snippet']['title']
-            description = item['snippet']['description']
-            thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
-            channel_id = item['snippet']['channelId']
-            channel_title = item['snippet']['channelTitle']
-            print(title)
-            Videos.objects.create(
-                video_id=video_id,
-                title=title,
-                description=description,
-                channel_id=channel_id,
-                channel_title=channel_title,
-                publishedDateTime=publishedDateTime,
-                thumbnailsUrls=thumbnailsUrls,
-            )
-        pass
-    
+        valid = False
+        youtubeKeys = os.getenv('YOUTUBE_KEY').split(",")
+        # print(youtubeKeys[0])
+        for key in youtubeKeys:
+            service = build('youtube', 'v3', developerKey=key)
+            result = service.search().list(part="snippet",q="how to make tea",type="video",order="date",publishedAfter="2010-01-01T00:00:00Z").execute()
+            valid = True
+            # result = service.search().list(part="snippet",q="tea how",type="video",order="date").execute()
+            # result = service.search().list(part="snippet",q="tea how",type="video",order="date", publishedAfter="2019").execute()
+            # response = request.execute()
+            # print(result)
+            if valid:
+                break
+
+        if valid:
+            for item in result['items']:
+                    video_id = item['id']['videoId']
+                    publishedDateTime = item['snippet']['publishedAt']
+                    title = item['snippet']['title']
+                    description = item['snippet']['description']
+                    thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
+                    channel_id = item['snippet']['channelId']
+                    channel_title = item['snippet']['channelTitle']
+                    print(title)
+                    Videos.objects.create(
+                        video_id=video_id,
+                        title=title,
+                        description=description,
+                        channel_id=channel_id,
+                        channel_title=channel_title,
+                        publishedDateTime=publishedDateTime,
+                        thumbnailsUrls=thumbnailsUrls,
+                    )
--- youtube_fetch/settings.py
+++ youtube_fetch/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xe(8tvsfcar)an#io@$i!_n%0t16dkc%rsgr2s28!)^un2epz9'
+# SECRET_KEY = 'xe(8tvsfcar)an#io@$i!_n%0t16dkc%rsgr2s28!)^un2epz9'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
