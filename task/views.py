7368449a404f75086c3c83cd6b590f8ef0aa1cba
Shivansh <gupta.shivansh98@gmail.com>
2020-09-24 10:23:08 +0530

    Added Pagination and Reverse sorting

--- task/views.py
+++ task/views.py
@@ -3,8 +3,22 @@ from rest_framework import viewsets
 from .serializers import VideoSerializer
 from .models import Videos
 
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination
+
 # Create your views here.
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
+class NodeResultsSetPagination(LimitOffsetPagination):
+    default_limit = 5
+    offset_query_param = 'skip'
+
 
 class VideoViewSet(viewsets.ModelViewSet):
-    queryset = Videos.objects.all().order_by('title')
+    # queryset = Videos.objects.all()
+    queryset = Videos.objects.all().order_by('publishedDateTime')
+    # queryset = Videos.objects.all().order_by('url')
     serializer_class = VideoSerializer
+    pagination_class = NodeResultsSetPagination
